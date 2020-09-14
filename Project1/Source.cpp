dbd08286c6d7b896b4b90a9ac19930becc93b721
Dokkaebi00 <quang009182@gmail.com>
2020-09-14 23:26:07 +0700

    Add project files.

---
+++ Project1/Source.cpp
@@ -0,0 +1,182 @@
+/* =============================================================
+INTRODUCTION TO GAME PROGRAMMING SE102
+
+SAMPLE 00 - INTRODUCTORY CODE
+
+This sample illustrates how to:
+
+1/ Create a window
+2/ Initiate DirectX 9, Direct3D, DirectX Sprite
+3/ Draw a static brick sprite to the screen
+
+WARNING: This example contains a hell LOT of *sinful* programming practices
+================================================================ */
+
+#include <windows.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+
+#include <signal.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define WINDOW_CLASS_NAME L"SampleWindow"
+#define MAIN_WINDOW_TITLE L"00 - Intro"
+
+#define BRICK_TEXTURE_PATH L"brick.png"
+
+#define BACKGROUND_COLOR D3DCOLOR_XRGB(255, 0, 0)
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+#define MAX_FRAME_RATE 60
+
+
+LPDIRECT3D9 d3d = NULL;						// Direct3D handle
+LPDIRECT3DDEVICE9 d3ddv = NULL;				// Direct3D device object
+
+LPDIRECT3DSURFACE9 backBuffer = NULL;
+LPD3DXSPRITE spriteHandler = NULL;			// Sprite helper library to help us draw 2D image on the screen 
+
+LPDIRECT3DTEXTURE9 texBrick;				// texture object to store brick image
+
+float brick_x = 0.0f;
+float brick_vx = 0.1f;
+float brick_y = 100.0f;
+
+#define _W(x)  __W(x)
+#define __W(x)  L##x
+
+#define VA_PRINTS(s) {				\
+		va_list argp;				\
+		va_start(argp, fmt);		\
+		vswprintf_s(s, fmt, argp);	\
+		va_end(argp);				\
+}	
+
+void DebugOut(const wchar_t* fmt, ...)
+{
+	wchar_t s[4096];
+	VA_PRINTS(s);
+	OutputDebugString(s);
+}
+
+//call-back function
+LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+//create window
+//1. register a window class
+//2. create window
+
+HWND CreateGameWindow(HINSTANCE hInstance, int nCmdShow, int ScreenWidth, int ScreenHeight)
+{
+	WNDCLASSEX wc;
+	
+	wc.cbSize = sizeof(WNDCLASSEX);
+
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.hInstance = hInstance;
+
+	wc.lpfnWndProc = (WNDPROC)WinProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hIcon = NULL;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = WINDOW_CLASS_NAME;
+	wc.hIconSm = NULL;
+
+	RegisterClassEx(&wc);
+
+	HWND hWnd = CreateWindow(
+		WINDOW_CLASS_NAME,
+		MAIN_WINDOW_TITLE,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		ScreenWidth,
+		ScreenHeight,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	if (!hWnd)
+	{
+		DWORD ErrCode = GetLastError();
+		DebugOut(L"[ERROR] CreateWindow failed! ErrCode: %d\nAt: %s %d \n", ErrCode, _W(__FILE__), __LINE__);
+		return 0;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return hWnd;
+}
+
+void Update(DWORD dt)
+{
+
+}
+
+void Render()
+{
+
+}
+
+int Run()
+{
+	MSG msg;
+	DWORD frameStart = GetTickCount();
+	DWORD tickPerFrame = 1000 / MAX_FRAME_RATE;
+	int done = 0;
+	while (!done)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+			{
+				done = 1;
+			}
+
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		DWORD now = GetTickCount();
+
+		DWORD dt = now - frameStart;
+
+		if (dt >= tickPerFrame)
+		{
+			frameStart = now;
+			Update(dt);
+			Render();
+		}
+		else
+		{
+			Sleep(tickPerFrame - dt);
+		}
+		
+	}
+
+	return 1;
+	
+}
